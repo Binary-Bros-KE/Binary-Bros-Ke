263f576dd57ab8312db58aa501be305cbec21663
theeWhizz <theewhizz2@gmail.com>
2023-12-31 07:35:16 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export { default as Contact } from './Contact';
+export { default as Home } from './Home';
+export { default as Pricing } from './Pricing';
+export { default as SEO } from './SEO';
+export { default as Services } from './Services';
+export { default as WebDev } from './WebDev';
