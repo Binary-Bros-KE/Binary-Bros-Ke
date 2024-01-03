3561febf9680894693cfcb5ed566fe693d176124
theeWhizz <theewhizz2@gmail.com>
2024-01-03 11:58:15 -0500

    blenick 3/1/2024

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -1,421 +1,91 @@
-import React from 'react'
+import styled from 'styled-components';
+import CardList from '../components/CardList';
+import SeoCardList2 from '../components/SeoCardList2';
+// import { Link } from 'react-router-dom'
+
+
+const StyledButton = styled.button`
+  padding: 0.5rem 2rem;
+  background-color: hsl(0, 0%, 0%);
+  color: #fff;
+  border-radius: 0.4em;
+  font-family: 'Poppins', sans;
+  transition: all 100ms ease-in-out;
+
+  &:hover {
+    color: #fff;
+    background-color: #0e232eff;
+  }
+`;
+
+const InverseButton = styled.button`
+  padding: 0.5rem 1.5rem;
+  color: hsl(0, 0%, 0%);
+  background-color: hsl(0, 0%, 87%);
+  border-radius: 0.4em;
+  margin-right: auto;
+  font-family: 'Poppins', sans;
+  transition: all 100ms ease-in-out;
+
+  &:hover {
+    color: #0084C8;
+  }
+`
 
 const Pricing = () => {
   return (
     <>
-      <section class="webDev__pricing-container py-4 md:py-10">
-        <div class="container-section">
-          <h1 class="text-4xl text-center text-primary font-bold py-10">
-            Website Development Pricing
-          </h1>
+      <div className="text-lg pricing-container">
 
-          <div class="webDev__pricing-container_cards even-columns">
-            <div
-              class="webDev__pricing-container_cards-card relative overflow-hidden bg-white py-5 px-5 border-2 rounded-xl"
-            >
-              <img
-                src="../img/symbols/Rectangle-2.svg"
-                class="absolute -top-1/4 -right-2/4"
-              />
-              <h2
-                class="text-xl font-semibold inline-block p-1 px-5 bg-neutral-100 rounded-3xl border-2"
-              >
-                Bronze
-              </h2>
-              <h1 class="text-4xl font-bold pt-6 pb-3">Ksh 18 000</h1>
-              <p class="text-sm py-2">
-                Perfect for new websites, personal portfolios and landing pages
-              </p>
-              <button
-                class="cta py-3 px-5 my-3 bg-blue-600 text-white w-full rounded-3xl duration-200 hover:opacity-60"
-              >
-                <a class="text-lg font-semibold" href="contact.html"
-                  >Get Started</a
-                >
-              </button>
-              <p class="my-3 text-lg font-bold">Consists of:</p>
-              <ul class="">
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">20 Pages Maximum</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">1 Domain Name Registered</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Bandwidth</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">
-                    Website Content Provided by The Client
-                  </p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Blog Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Website Security</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">2 Website Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Whatsapp Chatbot Integration</p>
-                </li>
-                <li class="flex items-center py-2">
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Picture and / Video Gallery</p>
-                </li>
-              </ul>
-            </div>
+      {/*====== Banner Container ========*/}
 
-            <div
-              class="webDev__pricing-container_cards-card relative overflow-hidden bg-white py-5 px-5 border-2 rounded-xl"
-            >
-              <img
-                src="../img/symbols/Rectangle-3.svg"
-                class="absolute -top-1/4 -right-2/4 opacity-40"
-              />
-              <h2
-                class="text-xl font-semibold inline-block p-1 px-5 bg-neutral-100 rounded-3xl border-2"
-              >
-                Gold
-              </h2>
-              <h1 class="text-4xl font-bold pt-6 pb-3">Ksh 18 000</h1>
-              <p class="text-sm py-2">
-                Perfect for new websites, personal portfolios and landing pages
-              </p>
-              <button
-                class="cta py-3 px-5 my-3 bg-blue-600 text-white w-full rounded-3xl duration-200 hover:opacity-60"
-              >
-                <a class="text-lg font-semibold" href="contact.html"
-                  >Get Started</a
-                >
-              </button>
-              <p class="my-3 text-lg font-bold">Consists of:</p>
-              <p class="my-3 text-lg font-bold">Everything in Basic</p>
-              <ul class="">
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">30 Pages Maximum</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">1 Domain Name Registered</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Bandwidth</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">
-                    Website Content Provided by The Client
-                  </p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Blog Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Website Security</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">4 Website Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Whatsapp Chatbot Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Picture and / Video Gallery</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">FAQ Section/Page</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Search Engine Optimization</p>
-                </li>
-                <li class="flex items-center py-2">
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Web Ads</p>
-                </li>
-              </ul>
-            </div>
-
-
-            <div
-              class="webDev__pricing-container_cards-card relative overflow-hidden bg-white py-5 px-5 border-2 rounded-xl"
-            >
-              <img
-                src="../img/symbols/Rectangle-1.svg"
-                class="absolute -top-1/4 -right-2/4 opacity-50"
-              />
-              <h2
-                class="text-xl font-semibold inline-block p-1 px-5 bg-neutral-100 rounded-3xl border-2"
-              >
-                Platinum
-              </h2>
-              <h1 class="text-4xl font-bold pt-6 pb-3">Ksh 18 000</h1>
-              <p class="text-sm py-2">
-                Perfect for new websites, personal portfolios and landing pages
-              </p>
-              <button
-                class="cta py-3 px-5 my-3 bg-blue-600 text-white w-full rounded-3xl duration-200 hover:opacity-60"
-              >
-                <a class="text-lg font-semibold" href="contact.html"
-                  >Get Started</a
-                >
-              </button>
-              <p class="my-3 text-lg font-bold">Consists of:</p>
-              <p class="my-3 text-lg font-bold">
-                Everything in Advanced and Basic
-              </p>
-              <ul class="">
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Number of Pages</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">1 Domain Name Registered</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Bandwidth</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">
-                    Website Content Provided by The Client
-                  </p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Blog Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Website Security</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Website Analytics</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Web Hosting</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Unlimited Website Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Whatsapp Chatbot Integration</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Picture and / Video Gallery</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">FAQ Section/Page</p>
-                </li>
-                <li
-                  class="flex items-center py-2 border-b-2 border-neutral-100"
-                >
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Search Engine Optimization</p>
-                </li>
-                <li class="flex items-center py-2">
-                  <i
-                    class="fa-regular fa-square-check"
-                    style="color: #4070f4"
-                  ></i>
-                  <p class="text-sm pl-4">Web Ads</p>
-                </li>
-              </ul>
+        <section>
+          <div className="container-section py-6 pt-16">
+            <div>
+              <h1 className='text-4xl sm:text-6xl font-semibold leading-tight pb-8 sm:w-[80%]'>We`ve got a plan <br /> that`s perfect for you</h1>
+              <div className="flex border-[0.15em] border-neutral-300/90 p-[0.2em] rounded-[0.4em] w-[19em]">
+                <StyledButton onClick={
+                  () => {
+                    const element = document.getElementById('webPrices');
+                    if (element) {
+                      element.scrollIntoView({ behavior: 'smooth' });
+                    }
+                  }
+                }>Web Prices</StyledButton>
+                <InverseButton onClick={
+                  () => {
+                    const element = document.getElementById('seoPrices');
+                    if (element) {
+                      element.scrollIntoView({ behavior: 'smooth' });
+                    }
+                  }
+                }>SEO Prices</InverseButton>
+              </div>
             </div>
           </div>
+        </section>
+
+      {/*====== Web Prices Section ========*/}
+
+        <section className="mb-10" id='webPrices'>
+          <div className="container-section">
+            <h1 className='text-3xl text-black-col font-semibold py-6'>Web Services Pricing</h1>
+            <CardList />
+          </div>
+        </section>
+
+      {/*====== SEO Prices Section ========*/}
+
+      <section className='seo padding-block bg-black' id='seoPrices'>
+        <div className="container-section">
+          <h1 className="text-3xl text-white font-semibold py-6">SEO Services Pricing</h1>
+          <SeoCardList2 />
         </div>
       </section>
+
+      {/*====== Footer Container ========*/}
+
+      </div>
     </>
   )
 }
